3b2985f3e31803fe8424ca6c61731773907d02ee
dhouglas <dhouglasbn@gmail.com>
2024-04-08 20:28:38 -0300

    feature: homepage html+css+navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Route, Routes  } from "react-router-dom";
 import "./styles/globals.css";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={SignIn} /> 
+        <Route path="/" Component={Home} />
+        <Route path="/signin" Component={SignIn} />
+        <Route path="/signup" Component={SignUp} />
+        <Route path="*" Component={Home} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,39 @@
+import workersImg from "../../assets/workers.svg";
+import taskLogo from "../../assets/task.svg";
+
+import "./styles.css";
+import { Link } from "react-router-dom";
+
 export default function Home() {
+
     return(
-        <>
-        <h1>Home</h1>
-        </>
+        <div className="homepage-container">
+        <header>
+            <Link to={"/"}>
+            <img className="task-logoimg" src={taskLogo} alt="Logomarca do serviço Task" />
+            </Link>
+            <div className="signin-signout">
+                <Link to={"/signin"}>
+                <button
+                className="login">Fazer Login</button>
+                </Link>
+                <Link to={'/signup'}>
+                <button className="signup-button">Comece a usar o Task agora</button>
+                </Link>
+            </div>
+        </header>
+        <main>
+            <h1>Organize as suas idéias.</h1>
+            <h2>Escreva listas de tarefas, gerencie atividades em equipes em um único espaço de trabalho.</h2>
+            <Link to={'/signup'}>
+                <button className="signup-button">Comece a usar o Task agora</button>
+            </Link>
+            <img
+                className="workers-svg"
+                src={workersImg}
+                alt="Ilustração simbolizando um time de desenvolvimento"
+            />
+        </main>
+        </div>
     )
 }
